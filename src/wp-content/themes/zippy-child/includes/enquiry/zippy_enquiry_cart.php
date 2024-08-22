3db74d03c93d7fd9f602d78e205076fb147b87eb
shin <nhathanh024@gmail.com>
2024-08-22 16:40:57 +0700

    update sass folder

--- src/wp-content/themes/zippy-child/includes/enquiry/zippy_enquiry_cart.php
+++ src/wp-content/themes/zippy-child/includes/enquiry/zippy_enquiry_cart.php
@@ -19,7 +19,7 @@ function product_variation_table_shortcode($atts)
   ob_start();
 ?>
   <div class="table-responsive" id="ProductVariationTable" style="margin: 30px 0">
-	  <h4>Table Variation</h4>
+    <h4>Table Variation</h4>
     <table class="product-variation-table">
       <thead>
         <tr>
@@ -33,32 +33,32 @@ function product_variation_table_shortcode($atts)
         </tr>
       </thead>
       <tbody>
-          <?php
-          foreach ($variations as $variation_id) {
-              $variation_obj = new WC_Product_Variation($variation_id);
-              $attributes_price = $variation_obj->get_price();
-              
-          ?>
-              <tr>
-                  <td><?php echo $variation_obj->get_sku(); ?></td>
-                  <?php
-                  foreach ($attributes as $attribute_name => $attribute) {
-                      $attribute_value = $variation_obj->get_attribute($attribute_name);
-                      echo '<td>' . esc_html($attribute_value ? $attribute_value : '-') . '</td>';
-                  }
-                  ?>
-                  <td style="display:flex;justify-content:center">
-                      <div class="quantity" data-price="<?php echo $attributes_price; ?>">
-                          <input type="button" value="-" class="qty_button minus">
-                          <input type="number" step="1" min="0" max="<?php echo $variation_obj->get_stock_quantity(); ?>" name="quantity" value="0" title="Qty" class="input-text qty text" size="4" data-variation-id="<?php echo $variation_obj->get_id(); ?>" data-price="<?php echo $variation_obj->get_price(); ?>" />
-                          <input type="button" value="+" class="qty_button plus">
-                      </div>
-                  </td>
-              </tr>
-          <?php
-              
-          }
-          ?>
+        <?php
+        foreach ($variations as $variation_id) {
+          $variation_obj = new WC_Product_Variation($variation_id);
+          $attributes_price = $variation_obj->get_price();
+
+        ?>
+          <tr>
+            <td><?php echo $variation_obj->get_sku(); ?></td>
+            <?php
+            foreach ($attributes as $attribute_name => $attribute) {
+              $attribute_value = $variation_obj->get_attribute($attribute_name);
+              echo '<td>' . esc_html($attribute_value ? $attribute_value : '-') . '</td>';
+            }
+            ?>
+            <td style="display:flex;justify-content:center">
+              <div class="quantity" data-price="<?php echo $attributes_price; ?>">
+                <input type="button" value="-" class="qty_button minus">
+                <input type="number" step="1" min="0" max="<?php echo $variation_obj->get_stock_quantity(); ?>" name="quantity" value="0" title="Qty" class="input-text qty text" size="4" data-variation-id="<?php echo $variation_obj->get_id(); ?>" data-price="<?php echo $variation_obj->get_price(); ?>" />
+                <input type="button" value="+" class="qty_button plus">
+              </div>
+            </td>
+          </tr>
+        <?php
+
+        }
+        ?>
       </tbody>
     </table>
   </div>
@@ -67,26 +67,26 @@ function product_variation_table_shortcode($atts)
   </div>
   <script>
     document.addEventListener('DOMContentLoaded', function() {
-//       document.querySelectorAll('.quantity').forEach(function(quantityWrapper) {
-//           var input = quantityWrapper.querySelector('input.qty');
-//           var minusButton = quantityWrapper.querySelector('.minus');
-//           var plusButton = quantityWrapper.querySelector('.plus');
+      //       document.querySelectorAll('.quantity').forEach(function(quantityWrapper) {
+      //           var input = quantityWrapper.querySelector('input.qty');
+      //           var minusButton = quantityWrapper.querySelector('.minus');
+      //           var plusButton = quantityWrapper.querySelector('.plus');
 
-//           minusButton.addEventListener('click', function() {
-//               var currentValue = parseInt(input.value, 10);
-//               if (!isNaN(currentValue) && currentValue > 0) {
-//                   input.value = currentValue - 1;
-//               }
-//           });
+      //           minusButton.addEventListener('click', function() {
+      //               var currentValue = parseInt(input.value, 10);
+      //               if (!isNaN(currentValue) && currentValue > 0) {
+      //                   input.value = currentValue - 1;
+      //               }
+      //           });
 
-//           plusButton.addEventListener('click', function() {
-//               var currentValue = parseInt(input.value, 10);
-//               var max = parseInt(input.getAttribute('max'), 10);
-//               if (!isNaN(currentValue) && (isNaN(max) || currentValue < max)) {
-//                   input.value = currentValue ;
-//               }
-//           });
-//       });
+      //           plusButton.addEventListener('click', function() {
+      //               var currentValue = parseInt(input.value, 10);
+      //               var max = parseInt(input.getAttribute('max'), 10);
+      //               if (!isNaN(currentValue) && (isNaN(max) || currentValue < max)) {
+      //                   input.value = currentValue ;
+      //               }
+      //           });
+      //       });
 
       document.getElementById('enquiry-button').addEventListener('click', function() {
         var quantities = document.querySelectorAll('.quantity input.qty');
@@ -101,9 +101,9 @@ function product_variation_table_shortcode($atts)
           var price = input.getAttribute('data-price');
           if (quantity > 0) {
             productsToAdd.enquiry.push({
-                variationId: variationId,
-                quantity: quantity
-              });
+              variationId: variationId,
+              quantity: quantity
+            });
           }
         });
 
@@ -116,7 +116,7 @@ function product_variation_table_shortcode($atts)
               products: productsToAdd
             },
             success: function(response) {
-              location.reload();
+              // location.reload();
             },
             error: function(error) {
               alert('Failed to process items.');
@@ -143,6 +143,11 @@ function add_multiple_to_cart_or_enquiry()
   if (isset($_POST['products']) && is_array($_POST['products'])) {
     $products = $_POST['products'];
 
+
+    // Early initialize customer session
+    if (isset(WC()->session) && ! WC()->session->has_session()) {
+      WC()->session->set_customer_session_cookie(true);
+    }
     // Process cart products
     if (!empty($products['cart'])) {
       foreach ($products['cart'] as $product) {
@@ -154,7 +159,7 @@ function add_multiple_to_cart_or_enquiry()
       }
     }
     // Process enquiry products
-     if (!empty($products['enquiry'])) {
+    if (!empty($products['enquiry'])) {
       foreach ($products['enquiry'] as $product) {
         $variation_id = intval($product['variationId']);
         $quantity = intval($product['quantity']);
@@ -163,16 +168,19 @@ function add_multiple_to_cart_or_enquiry()
           if (isset($enquiry_cart[$variation_id])) {
             $enquiry_cart[$variation_id]['quantity'] += $quantity;
           } else {
+
             $enquiry_cart[$variation_id] = array(
               'variation_id' => $variation_id,
               'quantity' => $quantity
             );
           }
+
           WC()->session->set('enquiry_cart', $enquiry_cart);
         }
       }
     }
-
+    $enquiry_cart = WC()->session->get('enquiry_cart', array());
+    // var_dump($enquiry_cart);
     wp_send_json_success('Products processed successfully.');
   } else {
     wp_send_json_error('No products to process.');
