7efd1b19750ff0ffdc388b2bfac5e350f60ff68f
shin <nhathanh024@gmail.com>
2024-08-23 09:16:18 +0700

    Demo js

--- src/wp-content/themes/zippy-child/assets/js/app.js
+++ src/wp-content/themes/zippy-child/assets/js/app.js
@@ -1,55 +1,48 @@
-// import { DisplayLabel } from './components/DisplayLabel';
+import { Enquiry } from "./enquiry/Enquiry";
 
 let Main = {
-  init: async function () {
-
-    // initialize demo javascript component - async/await invokes some 
-    //  level of babel transformation 
-    const displayLabel = new DisplayLabel();
-    await displayLabel.init();
-
-  }
+  init: function () {
+    const enquiry = new Enquiry();
+    enquiry.sortLengthList();
+  },
 };
 
-
-// console.log($('.more-link'));
-// Main.init();
-
+Main.init();
 
 // code JS to process sort variation product by length
 function sortLengthList() {
   const ul = document.querySelector('ul[role="radiogroup"]');
-  
+
   // Check if the ul element is found
   if (!ul) {
-      return;
+    return;
   }
 
-  const items = Array.from(ul.querySelectorAll('li'));
-  
+  const items = Array.from(ul.querySelectorAll("li"));
+
   // Check if the items list is empty
   if (items.length === 0) {
-      return;
+    return;
   }
 
   const sortedItems = items.sort((a, b) => {
-      const getLength = (item) => {
-          const value = item.getAttribute('data-value').toLowerCase();
-          if (value.includes('mm')) {
-              return parseFloat(value);
-          }
-          return 0;
-      };
+    const getLength = (item) => {
+      const value = item.getAttribute("data-value").toLowerCase();
+      if (value.includes("mm")) {
+        return parseFloat(value);
+      }
+      return 0;
+    };
 
-      return getLength(a) - getLength(b);
+    return getLength(a) - getLength(b);
   });
 
   // Clear existing items
-  ul.innerHTML = '';
+  ul.innerHTML = "";
 
   // Append the sorted items
-  sortedItems.forEach(item => {
-      ul.appendChild(item);
+  sortedItems.forEach((item) => {
+    ul.appendChild(item);
   });
-};
-sortLengthList()
+}
+// sortLengthList()
